5198aabf6e98bd6e03106961f5c43d40d0e89d9b
idemeu <idemeu.pedro@gmail.com>
2020-09-08 21:55:59 -0300

    segundo commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,13 @@ $('nav div a').click(function(e){
 		scrollTop: targetOffset - 55
 	}, 500);
 });
+
+$('section div div a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('header').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - 55
+	}, 500);
+});
